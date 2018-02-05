e649a763522e8565ed71b21caa33f29c32fd6a87
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-02-04 20:17:21 -0500

    2

---
+++ 1-10/2/2.hs
@@ -0,0 +1,20 @@
+import Control.Exception
+
+check :: String -> [Bool] -> IO ()
+check name tests = assert (and tests) $
+    putStrLn (name ++ ": tests passed")
+
+fibonacci :: [Int]
+fibonacci = 1 : 2 : zipWith (+) fibonacci (tail fibonacci)
+  
+checkFibonacci :: IO ()
+checkFibonacci = check "fibonacci"
+    [ take 10 fibonacci == [1,2,3,5,8,13,21,34,55,89]
+    , last (takeWhile (< 4000000) fibonacci) == 3524578
+    ]
+
+test :: IO ()
+test = checkFibonacci
+
+main :: IO ()
+main = print (sum (filter even (takeWhile (< 4000000) fibonacci)))
