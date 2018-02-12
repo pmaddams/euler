2b2b9c8b54ff52298a5b68e3e23d37ff6b7a413d
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-02-12 14:05:18 -0500

    sync definitions

--- 1-10/2/2.hs
+++ 1-10/2/2.hs
@@ -5,11 +5,11 @@ check name tests = assert (and tests) $
     putStrLn (name ++ ": tests passed")
 
 fibonacci :: [Integer]
-fibonacci = 1 : 2 : zipWith (+) fibonacci (tail fibonacci)
+fibonacci = 1 : 1 : zipWith (+) fibonacci (tail fibonacci)
   
 checkFibonacci :: IO ()
 checkFibonacci = check "fibonacci"
-    [ take 10 fibonacci == [1,2,3,5,8,13,21,34,55,89]
+    [ take 10 fibonacci == [1,1,2,3,5,8,13,21,34,55]
     , last (takeWhile (< 4000000) fibonacci) == 3524578
     ]
 
