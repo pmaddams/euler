4c834b8a3394c2bb229b1c79a9d2bc9cebfd8a24
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-02-14 17:09:38 -0500

    30

---
+++ 21-30/30/30.hs
@@ -0,0 +1,27 @@
+import Control.Exception
+import Data.Char
+
+check :: String -> [Bool] -> IO ()
+check name tests = assert (and tests) $
+    putStrLn (name ++ ": tests passed")
+
+isSumOfPowers :: Int -> Int -> Bool
+isSumOfPowers x n = s' (map digitToInt (show n)) 0
+  where
+    s' [] acc     = n == acc
+    s' (d:ds) acc =
+        if acc > n 
+        then False
+        else s' ds (d^x + acc)
+
+checkIsSumOfPowers :: IO ()
+checkIsSumOfPowers = check "isSumOfPowers"
+    [ not (isSumOfPowers 4 1234)
+    , and (map (isSumOfPowers 4) [1634,8208,9474])
+    ]
+
+test :: IO ()
+test = checkIsSumOfPowers
+
+main :: IO ()
+main = print (sum [n | n <- [2..1000000], isSumOfPowers 5 n])
