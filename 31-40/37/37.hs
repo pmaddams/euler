e2aade883fc7c5a21c70c6223785a1b962614f6a
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-02-18 21:20:08 -0500

    37

---
+++ 31-40/37/37.hs
@@ -0,0 +1,111 @@
+import Control.Exception
+import Data.Char
+import qualified Data.Map as M
+
+check :: String -> [Bool] -> IO ()
+check name tests = assert (and tests) $
+    putStrLn (name ++ ": tests passed")
+
+primes :: Integral a => [a]
+primes = sieve [2..] M.empty
+  where
+    sieve (n:ns) m = case M.lookup n m of
+        Nothing -> n : sieve ns (M.insert (n^2) [n] m)
+        Just ps -> sieve ns (foldl mark (M.delete n m) ps)
+      where
+        mark m p = M.insertWith (++) (n+p) [p] m
+
+checkPrimes :: IO ()
+checkPrimes = check "primes"
+    [ take 6 primes == [2,3,5,7,11,13]
+    , primes !! 999 == 7919
+    ]
+
+modExp :: Integer -> Integer -> Integer -> Integer
+modExp _ _ 1 = 0
+modExp b x m = m' (b `mod` m) x
+  where
+    m' _ 0 = 1
+    m' b x =
+        let b' = (b^2) `mod` m
+            x' = x `div` 2
+        in if x `mod` 2 == 1
+           then (b * m' b' x') `mod` m
+           else m' b' x'
+
+checkModExp :: IO ()
+checkModExp = check "modExp"
+    [ modExp 5 3 3 == 2
+    , modExp 2 10 3 == 1
+    ]
+
+millerRabin :: Integer -> [Integer] -> Bool
+millerRabin n []     = True
+millerRabin n (a:as) =
+    modExp a (n-1) n == 1 &&
+    millerRabin n as
+
+smallPrime :: Int -> Bool
+smallPrime n = n > 1 &&
+    let ps = take 4 primes
+    in n `elem` ps ||
+       all (\p -> n `mod` p /= 0) ps &&
+       millerRabin (toInteger n) (map toInteger ps)
+
+checkSmallPrime :: IO ()
+checkSmallPrime = check "smallPrime"
+    [ all smallPrime (take 100 primes)
+    , not (any smallPrime [-1,0,1,4,9])
+    ]
+
+toDigits :: Integral a => a -> [Int]
+toDigits = map digitToInt . show . fromIntegral
+
+checkToDigits :: IO ()
+checkToDigits = check "toDigits"
+    [ toDigits 12345 == [1..5]
+    , toDigits 987654321 == [9,8..1]
+    ]
+
+fromDigits :: Integral a => [Int] -> a
+fromDigits = fromIntegral . read . concatMap show
+ 
+checkFromDigits :: IO ()
+checkFromDigits = check "fromDigits"
+    [ fromDigits [1..5] == 12345
+    , fromDigits [9,8..1] == 987654321
+    ]
+
+truncatable :: Int -> Bool
+truncatable n = smallPrime n &&
+    let ds = toDigits n
+    in length ds > 1 &&
+        t' ds [] (reverse ds) []
+  where
+    t' [] _ [] _           = True
+    t' (f:fs) fa (r:rs) ra =
+        let fa' = fa ++ [f]
+            ra' = r : ra
+        in smallPrime (fromDigits fa') &&
+            smallPrime (fromDigits ra') &&
+            t' fs fa' rs ra'
+
+checkTruncatable :: IO ()
+checkTruncatable = check "truncatable"
+    [ truncatable 3797
+    , not (any truncatable [2,3,5,7])
+    ]
+
+test :: IO ()
+test = do
+    checkPrimes
+    checkModExp
+    checkSmallPrime
+    checkToDigits
+    checkFromDigits
+    checkTruncatable
+
+main :: IO ()
+main =
+    let ps = take 11 (filter truncatable primes)
+    in print (sum ps)
