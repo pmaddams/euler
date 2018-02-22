389b0fddb9667fadf42dd4baed426308c97b2f1d
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-02-22 12:41:10 -0500

    45

---
+++ 41-50/45/45.hs
@@ -0,0 +1,38 @@
+import Control.Exception
+import Data.List
+
+check :: String -> [Bool] -> IO ()
+check name tests = assert (and tests) $
+    putStrLn (name ++ ": tests passed")
+
+hexagons :: [Int]
+hexagons = [n*(2*n-1) | n <- [1..]]
+
+checkHexagons :: IO ()
+checkHexagons = check "hexagons"
+    [ take 5 hexagons == [1,6,15,28,45]
+    , 3003 `elem` hexagons
+    ]
+
+pentagonal :: Int -> Bool
+pentagonal n =
+    let r = sqrt (fromIntegral (24*n + 1))
+        (ri, rf) = properFraction r
+    in rf == 0 && ri `mod` 6 == 5
+
+checkPentagonal :: IO ()
+checkPentagonal = check "pentagonal"
+    [ all pentagonal [1,5,12,22,35]
+    , not (pentagonal 48)
+    ]
+
+test :: IO ()
+test = do
+    checkHexagons
+    checkPentagonal
+
+main :: IO ()
+main =
+    let hs = dropWhile (<= 40755) hexagons
+    in case (find pentagonal hs) of
+           Just n -> print n
