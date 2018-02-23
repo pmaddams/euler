66b83dfc232ac7a544b4c0f3f2e275f0e3fc17ea
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-02-22 21:42:33 -0500

    48

---
+++ 41-50/48/48.hs
@@ -0,0 +1,33 @@
+import Control.Exception
+
+check :: String -> [Bool] -> IO ()
+check name tests = assert (and tests) $
+    putStrLn (name ++ ": tests passed")
+
+modExp :: Integer -> Integer -> Integer -> Integer
+modExp _ _ 1 = 0
+modExp b x m = m' (b `mod` m) x
+  where
+    m' _ 0 = 1
+    m' b x =
+        let b' = (b^2) `mod` m
+            x' = x `div` 2
+        in if x `mod` 2 == 1
+           then (b * m' b' x') `mod` m
+           else m' b' x'
+
+checkModExp :: IO ()
+checkModExp = check "modExp"
+    [ modExp 5 3 3 == 2
+    , modExp 2 3 5 == 3
+    ]
+
+test :: IO ()
+test = checkModExp
+
+main :: IO ()
+main =
+    let ns = [1..1000]
+        m = 10^10
+        rs = zipWith3 modExp ns ns (repeat m)
+    in print (sum rs `mod` m)
