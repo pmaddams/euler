0c7618cbcf6ae1cc355a1ef02e65feae96cd48fd
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-02-11 19:05:12 -0500

    11

---
+++ 11-20/11/Main.hs
@@ -0,0 +1,54 @@
+-- 11. Largest product in a grid
+
+module Main where
+
+import Control.Monad
+import Data.Array
+
+type Coordinate = (Int, Int)
+type Direction = Coordinate -> Coordinate
+type Grid a = Array Coordinate a
+
+main :: IO ()
+main = do
+    grid <- readGrid "11.txt"
+    let ns = map product (connect 4 grid)
+    print (maximum ns)
+
+connect :: Int -> Grid a -> [[a]]
+connect n grid = do
+    start <- range (bounds grid)
+    dir <- [east, southeast, south, southwest]
+    let xs = (walk n grid dir start)
+    guard (not (null xs))
+    return xs
+
+walk :: Int -> Grid a -> Direction -> Coordinate -> [a]
+walk n grid dir start = do
+    let coords = take n (iterate dir start)
+    guard (all (inRange (bounds grid)) coords)
+    coord <- coords
+    return (grid ! coord)
+
+east :: Direction
+east (i, j) = (i, j+1)
+
+southeast :: Direction
+southeast (i, j) = (i+1, j+1)
+
+south :: Direction
+south (i, j) = (i+1, j)
+
+southwest :: Direction
+southwest (i, j) = (i+1, j-1)
+
+makeGrid :: [[a]] -> Grid a
+makeGrid xss@(xs:_) =
+    let m = length xss
+        n = length xs
+    in listArray ((1, 1), (m, n)) (concat xss)
+
+readGrid :: (Integral a, Read a) => FilePath -> IO (Grid a)
+readGrid name = readFile name >>= f
+  where
+    f = return . makeGrid . map (map read) . map words . lines
---
+++ 11-20/11/Test.hs
@@ -0,0 +1,41 @@
+module Test where
+
+import Data.List
+
+import Main hiding (main)
+
+grid :: Grid Int
+grid = makeGrid [[1,2,3],[4,5,6],[7,8,9]]
+
+main :: IO ()
+main = do
+    testConnect
+    testWalk
+
+testConnect :: IO ()
+testConnect = test "connect"
+    [ sort (connect 3 grid) ==
+          [ [1,2,3]
+          , [1,4,7]
+          , [1,5,9]
+          , [2,5,8]
+          , [3,5,7]
+          , [3,6,9]
+          , [4,5,6]
+          , [7,8,9]
+          ]
+    ]
+
+testWalk :: IO ()
+testWalk = test "walk"
+    [ walk 2 grid east (1, 2) == [2, 3]
+    , walk 2 grid southwest (2, 3) == [6, 8]
+    , walk 3 grid south (1, 1) == [1,4,7]
+    , walk 3 grid southeast (2, 2) == []
+    ]
+
+test :: String -> [Bool] -> IO ()
+test name cases =
+    if and cases
+    then return ()
+    else error name
