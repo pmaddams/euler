bb2cf02fe0269598bf54729ccc3f83978433ae04
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-02-13 18:57:29 -0500

    26

--- 21-30/26/Test.hs
+++ 21-30/26/Test.hs
@@ -3,13 +3,21 @@ module Test where
 import Main hiding (main)
 
 main :: IO ()
-main = testCycleLength
+main = do
+    testCycleLength
+    testBest
 
 testCycleLength :: IO ()
 testCycleLength = test "cycleLength"
     [ cycleLength 1 == 0
     , cycleLength 6 == 1
     , cycleLength 7 == 6
+    ]
+
+testBest :: IO ()
+testBest = test "best"
+    [ best [("a", 2), ("b", 3), ("c", 1)] == "b"
+    , best [(2, "a"), (3, "b"), (1, "c")] == 1
     ]
 
 test :: String -> [Bool] -> IO ()
