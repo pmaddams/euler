946b84bafbab6b27d2c5a4cda46e7a6f62170f88
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-02-14 18:12:12 -0500

    17

--- 11-20/17/Test.hs
+++ 11-20/17/Test.hs
@@ -7,7 +7,12 @@ main = testSay
 
 testSay :: IO ()
 testSay = test "say"
-    [
+    [ say 123 == "One Hundred and Twenty Three"
+    , say 54321 == "Fifty Four Thousand, Three Hundred and Twenty One"
+    , say 918273645 ==
+          "Nine Hundred and Eighteen Million, " ++
+          "Two Hundred and Seventy Three Thousand, " ++
+          "Six Hundred and Forty Five"
     ]
 
 test :: String -> [Bool] -> IO ()
