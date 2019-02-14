27dd2c3c3772ed425b9aad53217d6d1be92305e0
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-02-14 17:35:54 -0500

    4

--- 01-10/04/Main.hs
+++ 01-10/04/Main.hs
@@ -2,18 +2,20 @@
 
 module Main where
 
+import Data.Char
 import Data.List
 import Data.Maybe
 
 main :: IO ()
-main = print (fromJust (find palindrome ns))
+main = print (fromJust (find (palindrome . toDigits) ns))
   where
     ns = sortReverse (products [100..999])
 
-palindrome :: Show a => a -> Bool
-palindrome x =
-    let s = show x
-    in s == reverse s
+palindrome :: Eq a => [a] -> Bool
+palindrome xs = xs == reverse xs
+
+toDigits :: Integral a => a -> [Int]
+toDigits = map digitToInt . show . fromIntegral
 
 sortReverse :: Ord a => [a] -> [a]
 sortReverse = sortBy (flip compare)
--- 01-10/04/Test.hs
+++ 01-10/04/Test.hs
@@ -5,6 +5,7 @@ import Main hiding (main)
 main :: IO ()
 main = do
     testPalindrome
+    testToDigits
     testSortReverse
     testProducts
 
@@ -12,8 +13,14 @@ testPalindrome :: IO ()
 testPalindrome = test "palindrome"
     [ palindrome "abba"
     , not (palindrome "abab")
-    , palindrome 12321
-    , not (palindrome 12345)
+    , palindrome [1,2,3,2,1]
+    , not (palindrome [1..5])
+    ]
+
+testToDigits :: IO ()
+testToDigits = test "toDigits"
+    [ toDigits 12345 == [1..5]
+    , toDigits 987654321 == [9,8..1]
     ]
 
 testSortReverse :: IO ()
