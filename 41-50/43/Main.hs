9a663cb04d081194c94038100f536311d070f1c5
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-02-15 20:21:27 -0500

    43

---
+++ 41-50/43/Main.hs
@@ -0,0 +1,41 @@
+-- 43. Sub-string divisibility
+
+module Main where
+
+import Control.Monad
+import Data.Char
+import Data.List
+
+main :: IO ()
+main = print (sum ns)
+  where
+    dss = map divisibleDigits [1,2,3,5,7,11,13,17]
+    ns = map fromDigits (filter unique (foldr1 mergeDigits dss))
+
+divisibleDigits :: Integral a => a -> [[Int]]
+divisibleDigits n =
+    let ms = takeWhile (< 1000) (map (n*) [1..])
+    in map (loop . toDigits) ms
+  where
+    loop ds = case (length ds) of
+        1 -> 0:0:ds
+        2 -> 0:ds
+        3 -> ds
+
+mergeDigits :: [[Int]] -> [[Int]] -> [[Int]]
+mergeDigits begins ends = concat (loop <$> begins <*> pure ends)
+  where
+    loop _ []                = []
+    loop bs@(b:bs') (es:ess) =
+        if bs' == take 2 es
+        then (b:es) : loop bs ess
+        else loop bs ess
+
+unique :: Eq a => [a] -> Bool
+unique xs = nub xs == xs
+
+toDigits :: Integral a => a -> [Int]
+toDigits = map digitToInt . show . fromIntegral
+
+fromDigits :: Integral a => [Int] -> a
+fromDigits = fromIntegral . read . map intToDigit
---
+++ 41-50/43/Test.hs
@@ -0,0 +1,52 @@
+module Test where
+
+import Main hiding (main)
+
+main :: IO ()
+main = do
+    testDivisibleDigits
+    testMergeDigits
+    testUnique
+    testToDigits
+    testFromDigits
+
+testDivisibleDigits :: IO ()
+testDivisibleDigits = test "divisibleDigits"
+    [ divisibleDigits 30 ==
+          [0,3,0] : [0,6,0] : [0,9,0] : map toDigits [120,150..990]
+    , divisibleDigits 300 == map toDigits [300,600,900]
+    ]
+
+testMergeDigits :: IO ()
+testMergeDigits = test "mergeDigits"
+    [ mergeDigits [[1,2,3],[4,5,6]] [[2,3,5,7],[5,6,8,10]] ==
+          [[1,2,3,5,7],[4,5,6,8,10]]
+    , mergeDigits [[1,2,3],[4,5,6]] [[2,3,5,7],[2,3,4,5,6,7]] ==
+          [[1,2,3,5,7],[1..7]]
+    ]
+
+testUnique :: IO ()
+testUnique = test "unique"
+    [ unique [1..5]
+    , not (unique [1,2,3,2,5])
+    , unique "cat"
+    , not (unique "doog")
+    ]
+
+testToDigits :: IO ()
+testToDigits = test "toDigits"
+    [ toDigits 12345 == [1..5]
+    , toDigits 987654321 == [9,8..1]
+    ]
+
+testFromDigits :: IO ()
+testFromDigits = test "fromDigits"
+    [ fromDigits [1..5] == 12345
+    , fromDigits [9,8..1] == 987654321
+    ]
+
+test :: String -> [Bool] -> IO ()
+test name cases =
+    if and cases
+    then return ()
+    else error name
