54bf246199ac7ff39dad2063e9204531e85ecf61
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-02-17 18:54:03 -0500

    47

---
+++ 41-50/47/Main.hs
@@ -0,0 +1,38 @@
+-- 47. Distinct primes factors
+
+module Main where
+
+import Data.Function
+import Data.List
+import qualified Data.Map as M
+
+main :: IO ()
+main = print $
+    let ns = [1..]
+        gs = groupBy ((==) `on` snd) (zip ns (map f ns))
+    in head [n | (n, m) <- map head (filter p gs), m == 4]
+  where
+    f = length . nub . factors
+    p = (>= 4) . length
+
+factors :: Integral a => a -> [a]
+factors n = loop n primes
+  where
+    loop n ps@(p:ps')
+        | n < 2         = []
+        | n < p^2       = [n]
+        | divisible n p = p : loop (quot n p) ps
+        | otherwise     = loop n ps'
+
+primes :: Integral a => [a]
+primes = loop [2..] M.empty
+  where
+    loop (n:ns) m = case (M.lookup n m) of
+        Nothing -> let m' = M.insert (n^2) [n] m
+                   in n : loop ns m'
+        Just ps -> let f p = M.insertWith (++) (n+p) [p]
+                       m' = foldr f (M.delete n m) ps
+                   in loop ns m'
+
+divisible :: Integral a => a -> a -> Bool
+divisible n d = rem n d == 0
---
+++ 41-50/47/Test.hs
@@ -0,0 +1,37 @@
+module Test where
+
+import Main hiding (main)
+
+main :: IO ()
+main = do
+    testFactors
+    testPrimes
+    testDivisible
+
+testFactors :: IO ()
+testFactors = test "factors"
+    [ factors 4 == [2,2]
+    , factors 5 == [5]
+    , factors 6 == [2,3]
+    , factors 7 == [7]
+    ]
+
+testPrimes :: IO ()
+testPrimes = test "primes"
+    [ take 5 primes == [2,3,5,7,11]
+    , primes !! 99 == 541
+    ]
+
+testDivisible :: IO ()
+testDivisible = test "divisible"
+    [ divisible 4 2
+    , not (divisible 7 3)
+    , divisible 10 5
+    , not (divisible 13 7)
+    ]
+
+test :: String -> [Bool] -> IO ()
+test name cases =
+    if and cases
+    then return ()
+    else error name
